2bb3e5c1a92f12faaa8edbea1ff1e746219b7178
mayank-for-learn-99 <1999mmmayank@gmail.com>
2020-02-05 22:07:13 +0530

    Initial commit

---
+++ Fibonacci_Dynamic_Approch.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Fibonacci_Dynamic_Approch{//Dynamic Programming
+	static ArrayList<Long> arr = new ArrayList<Long>();
+	static long fib(int n)
+	{
+		arr.add(1L);
+		arr.add(1L);
+		arr.add(1L);
+				for(int i=3;i<=n;i++)
+			    { 
+					long res =arr.get(i-1)+arr.get(i-2);
+			        arr.add(i,res);
+				}
+			return arr.get(n);
+	}
+	public static void main(String [] args){
+	
+		int n=500;
+		long sum=fib(n);
+		System.out.println(sum);
+			
+	}
+	}
